2767919504e55fa43932f497858b352ec8e1e44a
Emimint <90863470+Emimint@users.noreply.github.com>
2024-04-06 12:27:21 -0400

    create menu-component

--- front-cinepro/src/app/app.module.ts
+++ front-cinepro/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
