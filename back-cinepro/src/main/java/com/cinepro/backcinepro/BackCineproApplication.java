db73eabd7a00257bd62a6727c3bb3b2dbaef8da2
Emimint <90863470+Emimint@users.noreply.github.com>
2024-04-06 10:50:26 -0400

    create backend project folder

---
+++ back-cinepro/src/main/java/com/cinepro/backcinepro/BackCineproApplication.java
@@ -0,0 +1,13 @@
+package com.cinepro.backcinepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackCineproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackCineproApplication.class, args);
+	}
+
+}
