00d26968cfa8169277dd784564051acd38a18a0d
Emimint <90863470+Emimint@users.noreply.github.com>
2024-04-06 12:49:07 -0400

    create accueil component

--- front-cinepro/src/app/app.module.ts
+++ front-cinepro/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
