8d909789754007cf9c45111c2871de12a71d89b7
Doupmat <doupmat@gmail.com>
2024-04-14 16:40:40 -0400

    create food section component doup

--- front-cinepro/src/app/app.module.ts
+++ front-cinepro/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MovieExperienceComponent } from './movie-experience/movie-experience.co
 import { PaymentUserComponent } from './payment-user/payment-user.component';
 import { ShowSelectionComponent } from './show-selection/show-selection.component';
 import { ShoppingCartModalComponent } from './shopping-cart-modal/shopping-cart-modal.component';
+import { FoodSectionComponent } from './food-section/food-section.component';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { ShoppingCartModalComponent } from './shopping-cart-modal/shopping-cart-
     PaymentUserComponent,
     ShowSelectionComponent,
     ShoppingCartModalComponent,
+    FoodSectionComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
