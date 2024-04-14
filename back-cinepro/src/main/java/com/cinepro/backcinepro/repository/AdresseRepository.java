663707b346663681321101a8f3ee593312b17a7f
cylia93 <cylia.oudiai@outlook.com>
2024-04-14 15:53:47 -0400

    create repository address

---
+++ back-cinepro/src/main/java/com/cinepro/backcinepro/repository/AdresseRepository.java
@@ -0,0 +1,9 @@
+package com.cinepro.backcinepro.repository;
+
+
+import com.cinepro.backcinepro.entity.Adresse;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdresseRepository extends JpaRepository<Adresse, Long> {
+
+}
