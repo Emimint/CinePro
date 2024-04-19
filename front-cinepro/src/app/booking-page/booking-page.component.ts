005660069b650d75699bd43bb00bc642d4ca36a7
cylia93 <cylia.oudiai@outlook.com>
2024-04-18 22:51:45 -0400

    update booking page

--- front-cinepro/src/app/booking-page/booking-page.component.ts
+++ front-cinepro/src/app/booking-page/booking-page.component.ts
@@ -1,15 +1,33 @@
-import { Component,OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
-declare function initSeatMap();
+//declare function initSeatMap();
 @Component({
   selector: 'app-booking-page',
   templateUrl: './booking-page.component.html',
   styleUrl: './booking-page.component.css'
 })
-export class BookingPageComponent implements OnInit {
+export class BookingPageComponent {
    
-  ngOnInit(){
-    initSeatMap();
+  // ngOnInit(){
+  //   initSeatMap();
+  // }
+  currentStep: number = 1;
+
+  displayStep(step: number) {
+    this.currentStep = step;
+  }
+
+  nextStep() {
+    if (this.currentStep < 3) {
+      this.currentStep++;
+    }
+  }
+
+  prevStep() {
+    if (this.currentStep > 1) {
+      this.currentStep--;
+    }
   }
   
+
 }
