74f5c9e1744eab7c96402e05f33a95deac87c048
Emimint <90863470+Emimint@users.noreply.github.com>
2024-04-20 16:29:10 -0400

    add movie-description component to routes

--- front-cinepro/src/app/app-routing.module.ts
+++ front-cinepro/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { ContactComponent } from './contact/contact.component';
 import { ShowSelectionComponent } from './show-selection/show-selection.component';
 import { BookingPageComponent } from './booking-page/booking-page.component';
 import { AboutPageComponent } from './about-page/about-page.component';
- 
+import { MovieDescriptionComponent } from './movie-description/movie-description.component';
+
 const routes: Routes = [
   { path: '', redirectTo: '/accueil', pathMatch: 'full' },
   { path: 'accueil', component: AccueilComponent },
@@ -13,8 +14,9 @@ const routes: Routes = [
   { path: 'show-selection', component: ShowSelectionComponent },
   { path: 'booking', component: BookingPageComponent },
   { path: 'about', component: AboutPageComponent },
+  { path: 'description', component: MovieDescriptionComponent },
 ];
- 
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
