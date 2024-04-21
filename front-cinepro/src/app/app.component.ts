ea396caaa43a3b358092ad7a5ac7bf46aaae96b7
Emimint <90863470+Emimint@users.noreply.github.com>
2024-04-21 15:30:06 -0400

    add geolocalisation at app booting

--- front-cinepro/src/app/app.component.ts
+++ front-cinepro/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'front-cinepro';
+
+  ngOnInit(): void {
+    if (!navigator.geolocation) {
+      alert(
+        'Vous devez activer la géolocalisation dans les paramètres de votre navigateur.'
+      );
+      return;
+    } else {
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log('latitude: ', position.coords.latitude);
+        console.log('longitude: ', position.coords.longitude);
+      });
+    }
+  }
 }
