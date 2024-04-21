f8f4f6bc9baf5d8f714b6578ca0f7443c95265a4
Emimint <90863470+Emimint@users.noreply.github.com>
2024-04-21 13:28:08 -0400

    add timer logic to booking component

---
+++ front-cinepro/src/app/time-format.pipe.ts
@@ -0,0 +1,20 @@
+// time-format.pipe.ts
+
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'timeFormat',
+})
+export class TimeFormatPipe implements PipeTransform {
+  transform(value: number): string {
+    const minutes: number = Math.floor(value / 60);
+    const seconds: number = value - minutes * 60;
+
+    const formattedMinutes: string =
+      minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const formattedSeconds: string =
+      seconds < 10 ? `0${seconds}` : `${seconds}`;
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+  }
+}
